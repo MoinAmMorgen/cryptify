b61a5110f77de47056c513d06fa3b28e729522c1
MoinAmMorgen <niklaspfeffer1@gmx.de>
2025-02-14 19:47:14 +0100

    create general structure

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 // import { invoke } from "@tauri-apps/api/core";
 
-
 window.addEventListener("DOMContentLoaded", () => {
-  
+  const buttons = document.querySelectorAll("#buttons button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      buttons.forEach((btn) => btn.classList.remove("active"));
+      button.classList.add("active");
+    });
+  });
 });
