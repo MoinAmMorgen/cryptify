1f5836cf0e142689dd36ad5cb000df24f6d09006
MoinAmMorgen <niklaspfeffer1@gmx.de>
2025-02-15 16:24:22 +0100

    add caesar encryption

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,51 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-// #[tauri::command]
+fn create_secret_alphabet(word: &str) -> Vec<char>{
+    let mut secret_alphabet: Vec<char> = Vec::new();
+    for ch in word.chars() {
+        if !secret_alphabet.contains(&ch) {
+            secret_alphabet.push(ch);
+        }
+    }
+    let mut start: u32 = 0;
+    if let Some(last_char) = word.chars().last() {
+        start = last_char as u32;
+    }
+    for i in 0..26 {
+        let mut temp = start + i;
+        if temp > 90 {
+            temp -= 26;
+        };
+        if let Some(character) = char::from_u32(temp) {
+            if !secret_alphabet.contains(&character) {
+                secret_alphabet.push(character);
+            }
+        }
+
+    }
+    return secret_alphabet;
+}
+
+#[tauri::command]
+fn caesar_encrypt(text: &str, key: &str) -> String{
+    let mut encrypted = String::new();
+    let secret_alphabet = create_secret_alphabet(key);
+    for ch in text.chars() {
+        let pos = (ch as u32) - 65; // NOTE: Will crash if special charcacters are included
+        if pos > 26 { // Only uppercase letters for now. TODO: Implement all letters and special characters
+            return "ERROR ENCRYPTING".to_string()
+        }
+        encrypted.push(secret_alphabet[pos as usize]);
+    }
+    return encrypted;
+}
+
 
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![])
+        .invoke_handler(tauri::generate_handler![caesar_encrypt])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
-// import { invoke } from "@tauri-apps/api/core";
+import { invoke } from "@tauri-apps/api/core";
 
 window.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll("#buttons button");
+  const buttons: NodeListOf<HTMLButtonElement> =
+    document.querySelectorAll("#buttons button");
+  const inputForm: HTMLFormElement | null =
+    document.querySelector("#input-form");
+  const inputTextarea: HTMLTextAreaElement | null =
+    document.querySelector("#input");
+  let output = document.querySelector("#output") as HTMLTextAreaElement;
+
+  async function caesar_encrypt() {
+    if (output) {
+      output.value = await invoke("caesar_encrypt", {
+        text: inputTextarea?.value,
+        key: "PLACEHOLDER", // TODO: Create user input for custom key or generate a custom one
+      });
+    }
+  }
 
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
@@ -9,4 +24,27 @@ window.addEventListener("DOMContentLoaded", () => {
       button.classList.add("active");
     });
   });
+
+  if (inputTextarea) {
+    inputTextarea.addEventListener("keydown", (e: KeyboardEvent) => {
+      if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        if (inputForm) {
+          inputForm.requestSubmit();
+          caesar_encrypt();
+        }
+      }
+    });
+  }
+
+  if (inputForm) {
+    inputForm.addEventListener("submit", (e: Event) => {
+      e.preventDefault();
+      if (inputTextarea) {
+        // Form submitted TODO: Add encrypion logic
+        // For Caesar only without possibility to switch:
+        caesar_encrypt();
+      }
+    });
+  }
 });
